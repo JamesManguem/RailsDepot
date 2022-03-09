8edad52c5b4093d5f0e19d3795a10d10b6384247
JamesManguem <18090067@alumno.utmetropolitana.edu.mx>
2022-03-09 09:39:49 -0500

    Add-SideBar

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -17,7 +17,7 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "prduct attributes must not be empty" do
+  test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
     assert product.errors[:title].any?
